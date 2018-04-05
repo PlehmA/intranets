2e79cd4b31029e3d1f222104519eb2a322e424c4
plehma <polachgg@gmail.com>
2018-04-05 16:07:46 -0300

    asdsad
    assadsadas

---
+++ app/Http/Controllers/MiController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MiController extends Controller
+{
+    public function index()
+    {
+      return view('roundcubemail');
+    }
+}
